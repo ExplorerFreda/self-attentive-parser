2360243f41af43564946f4d0bf0029467c2cb420
Freda <freda@ttic.edu>
2019-10-16 21:36:09 -0500

    [master] update training script

--- scripts/train_bert_data-eff.sh
+++ scripts/train_bert_data-eff.sh
@@ -1,6 +1,7 @@
 cd ..
+mkdir models/en_bert-$1p/
 python src/main.py train \
     --train-path data/02-21.10way.clean.$1p \
-    --model-path-base models/en_bert_only-$1p/model-$2 \
+    --model-path-base models/en_bert-$1p/model-$2 \
     --bert-model "bert-large-uncased" --learning-rate 0.00005 --batch-size 32 --eval-batch-size 16 --subbatch-max-tokens 500 \
-    --use-bert --use-bert-only --evalb-dir EVALB_SPMRL/
+    --use-bert --evalb-dir EVALB_SPMRL/
