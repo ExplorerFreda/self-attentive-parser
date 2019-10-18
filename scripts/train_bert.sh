d40fc49d0ed7ce7db56457a94a5fa9bb8a6d8ec9
Freda <freda@ttic.edu>
2019-10-18 18:58:58 -0500

    [master] update scripts

--- scripts/train_bert.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-for i in 1 2 3 4 5
-do
-    python src/main.py train --use-bert --model-path-base models/en_benepar/model-$i --bert-model "bert-large-uncased" --num-layers 2 \
-        --learning-rate 0.00005 --batch-size 32 --eval-batch-size 16 --subbatch-max-tokens 500
-done
-
