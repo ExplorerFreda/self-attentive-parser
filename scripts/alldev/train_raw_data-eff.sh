7969501d121de4f45d7c5026b4b7971d3cc9655e
Freda <freda@ttic.edu>
2019-10-18 19:01:34 -0500

    [master] correct paths

--- scripts/alldev/train_raw_data-eff.sh
+++ scripts/alldev/train_raw_data-eff.sh
@@ -1,4 +1,4 @@
-cd ..
+cd ...
 python src/main.py train --model-path-base models/en_benepar_raw/model-$1p-$2 --num-layers 2 \
     --train-path data/02-21.10way.clean.$1p --use-words --use-chars-lstm --d-char-emb 64 \
     --evalb-dir EVALB_SPMRL/ \
